a6bc05f64b964efe7d3d921502177005f9f7fbec
kjetilthogersen <kjetil.thogersen@fys.uio.no>
2022-03-19 11:33:08 +0100

    fix for error in combining U_SSA and U_SIA

--- pyGlacier/Flowline.py
+++ pyGlacier/Flowline.py
@@ -11,7 +11,7 @@ from .Output import Output
 class Flowline:
 	#
 	# Solves a flowline model based on SIA and/or SIA. Semi-implicit approach for the advection diffusion equation when SSA is active. SIA is solved implicityly. The integration step is performed with backward Euler (explicit for the advection term).
-	# 
+	#
 
 	def __init__(self, variables):
 
@@ -66,7 +66,7 @@ class Flowline:
 			self.FrictionLaw = None
 		else:
 			raise Exception('FrictionLaw keyword not recognized')
-		
+
 		if self.Output == 'standard':
 			print(self.Output)
 			self.Output = Output(foldername = variables['Output']['foldername'], output_interval = variables['Output']['output_interval'], flush_interval = variables['Output']['flush_interval'], file_format = variables['Output']['file_format'], model = self, reduced = variables['Output']['reduced'], reduced_output_interval = variables['Output']['reduced_output_interval'])
@@ -100,11 +100,7 @@ class Flowline:
 		# the shallow ice approximation and the shallow shelf approximation as well as a given surface mass balance
 		h = self.H+self.b
 		D_staggered = (self.D[1:]+self.D[0:-1])/2.0 #Staggered grid
-
-
 		h = step_advection_diffusion_1d(dt = self.dt, dx = self.dx, phi = h, sourceTerm = self.SMB(self), D = D_staggered, Vphi = -self.sliding_velocity*self.H, left_boundary_condition = self.left_bc_type, left_boundary_condition_value = self.left_bc_val, right_boundary_condition = self.right_bc_type, right_boundary_condition_value = self.right_bc_val)
-		#h = step_advection_diffusion_1d(dt = self.dt, dx = self.dx, phi = h, sourceTerm = self.SMB(h), D = D_staggered, Vphi = -self.sliding_velocity*self.H, left_boundary_condition = 'vonNeuman',left_boundary_condition_value = 0.0, right_boundary_condition = 'Dirichlet', right_boundary_condition_value = self.b[-1])
-		#h = step_diffusion_1d(dt = self.dt, dx = self.dx, phi = h, sourceTerm = self.SMB, D = D_staggered, left_boundary_condition = 'vonNeuman',left_boundary_condition_value = 0.0, right_boundary_condition = 'vonNeuman', right_boundary_condition_value = 0.0)
 		self.H = h-self.b
 		self.H[np.where(self.H<=0)] = 0 # Ice thickness cannot be below zero
 
@@ -116,16 +112,15 @@ class Flowline:
 
 		if((self.solver == 'SSA') or (self.solver == 'coupled')):
 			self.update_ssa_flowline()
-		
+
 		if(self.solver == 'coupled'):
-			self.U = (1.0 - self.superposed(self.u_SIA))*self.u_SIA + self.superposed(self.u_SSA)*self.u_SSA
-			self.U = self.u_SIA + self.u_SSA
-			self.sliding_velocity = self.u_SSA
+			self.U = (1.0 - self.superposed(self.u_SSA))*self.u_SIA + self.superposed(self.u_SSA)*self.u_SSA
+			self.sliding_velocity = self.superposed(self.u_SSA)*self.u_SSA
 			self.D = (1.0 - self.superposed(self.u_SIA))*self.D_SIA
 			self.D = self.D_SIA
 
 		elif(self.solver == 'SIA'):
-			self.U = self.u_SIA 
+			self.U = self.u_SIA
 			self.sliding_velocity = np.zeros(np.size(self.u_SIA))
 			self.D = self.D_SIA
 
@@ -133,7 +128,7 @@ class Flowline:
 			self.U = self.u_SSA
 			self.sliding_velocity = self.u_SSA
 			self.D = np.zeros(np.size(self.D_SSA))
-		
+
 		if((self.solver == 'SSA') or (self.solver == 'coupled')):
 			self.FrictionLaw.step()
 			self.DrainageSystem.step()
@@ -142,11 +137,11 @@ class Flowline:
 
 		if(self.Output != 'None'):
 			self.Output.save()
-		
+
 		self.t = self.t + self.dt
 
 	def runAdaptive(self,t_max,dt_min, dt_max, error_tolerance, interval): #TODO: make sure the error measures will work for all choices of models
-		
+
 		print('********** RUN ADAPTIVE **********')
 		print('t max: ' + str(t_max/secondyears) + ' years')
 		print('dt in [' + str(dt_min) + ',' + str(dt_max) +'] s')
@@ -159,7 +154,7 @@ class Flowline:
 		self.dt = dt_min
 		while self.t<t_max:
 			i+=1
-			
+
 			if(i%interval==0):
 				model_copy = copy.deepcopy(self)
 				model_copy.Output ='None'
@@ -219,7 +214,7 @@ class Flowline:
 
 
 	def runInitializeSIA(self,dt,t_max):
-		
+
 		print('********** INITIALIZE SIA **********')
 
 		# Initialize thickness with SIA solution (no sliding or drainage)
@@ -242,7 +237,7 @@ class Flowline:
 		else:
 			dictionary = {'U':self.U.tolist(), 'H':self.H.tolist(), 't':self.t, 'length':np.sum(self.H>0)*self.dx}
 			if self.solver == 'coupled':
-				dictionary.update({'U_SIA':self.u_SIA.tolist(), 'U_SSA':self.u_SSA.tolist()})
+				dictionary.update({'U_SIA':self.u_SIA.tolist(), 'U_SSA':self.u_SSA.tolist(), 'sliding_velocity':self.sliding_velocity.tolist()})
 			return dictionary
 
 	def update_sia_flowline(self):
@@ -253,7 +248,9 @@ class Flowline:
 		h_bc = np.hstack([h[0],h,h[-1]])
 		dhdx = (h_bc[1:]-h_bc[0:-1])/self.dx
 		dhdx = (dhdx[1:]+dhdx[0:-1])/2.0
-		self.u_SIA = -2.0*(self.rho*self.g)**self.n *self.A*(1.0/(self.n+1.0))*dhdx**self.n *self.H**(self.n+1.0)
+
+		#self.u_SIA = -2.0*(self.rho*self.g)**self.n *self.A*(1.0/(self.n+1.0))*dhdx**self.n *self.H**(self.n+1.0) # This is the surface velocity
+		self.u_SIA = -2.0*(self.rho*self.g)**self.n *self.A*(1.0/(self.n+2.0))*dhdx**self.n *self.H**(self.n+1.0)
 		self.D_SIA = 2.0*(self.rho*self.g)**self.n *self.A*(1.0/(self.n+2.0))*dhdx**(self.n-1.0) *self.H**(self.n+2.0)
 
 
@@ -264,7 +261,7 @@ class Flowline:
 		h = self.H+self.b
 		dhdx = np.gradient(h)/self.dx
 		driving_stress = self.rho*self.g*self.H*dhdx
-		
+
 		rel_error = 1.0
 		i = 0
 
@@ -279,7 +276,7 @@ class Flowline:
 
 			dudx = (u[1:] - u[0:-1])/self.dx # On staggered grid
 			dudx_sqr_reg = dudx**2.0 + eps**2.0
-			
+
 			W = 2*self.A**(-1.0/self.n)*(self.H[1:] + self.H[0:-1])/2.0*dudx_sqr_reg**(((1.0/self.n)-1.0)/2.0)
 			W = np.hstack([W,W[-1]])
 			u_new = step_elliptical_1d(dx = self.dx, W = W, alpha = alpha, beta = driving_stress, gamma = driving_stress*0, left_boundary_condition = 'Dirichlet',left_boundary_condition_value = 0.0, right_boundary_condition = 'Dirichlet', right_boundary_condition_value = 0.0)
@@ -287,7 +284,7 @@ class Flowline:
 
 			u = u_new
 			i = i+1
-		
+
 		if(i==itermax):
 			print('warning: maximum number of iterations reached in SSA solver')
 
